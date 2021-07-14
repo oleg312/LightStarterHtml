5c767e228862867bd67c60b1dc77971751c07e65
Oleh <oleh19127@gmail.com>
2021-07-14 23:31:48 +0300

    add basics structure for JS

--- #src/assets/js/main.js
+++ #src/assets/js/main.js
@@ -1,23 +1,108 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//   // IBG METHOD
-//   function ibg() {
-//     let ibg = document.querySelectorAll(".ibg");
-//     for (var i = 0; i < ibg.length; i++) {
-//       if (ibg[i].querySelector('img')) {
-//         ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-//       }
-//     }
-//   }
-//   ibg();
-// });
+document.addEventListener('DOMContentLoaded', function () {
 
-// // PRELOADER
+   const menuLinksData = document.querySelectorAll('.menu__link[data-goto]')
+   const iconMenu = document.querySelector('.menu__icon')
+   const menuBody = document.querySelector('.menu__body')
+   const menuLinks = document.querySelectorAll('.menu__link')
+
+   // CHECK THE DEVICE
+   const isMobile = {
+      Android: function () {
+         return navigator.userAgent.match(/Android/i)
+      },
+      BlackBerry: function () {
+         return navigator.userAgent.match(/BlackBerry/i)
+      },
+      IOS: function () {
+         return navigator.userAgent.match(/iPhone|iPad|iPod/i)
+      },
+      Opera: function () {
+         return navigator.userAgent.match(/Opera Mini/i)
+      },
+      Windows: function () {
+         return navigator.userAgent.match(/IEMobile/i)
+      },
+      any: function () {
+         return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.IOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+      }
+   }
+
+   if (isMobile.any()) {
+      document.body.classList.add('_touch')
+      let menuArrows = document.querySelectorAll('.menu__arrow')
+      if (menuArrows.length > 0) {
+         for (let index = 0; index < menuArrows.length; index++) {
+            const menuArrow = menuArrows[index];
+            menuArrow.onclick = (() => {
+               menuArrow.parentElement.classList.toggle('_active')
+            })
+
+         }
+      }
+   } else {
+      document.body.classList.add('_pc')
+   }
+
+   // SCROLL
+   if (menuLinksData) {
+      menuLinksData.forEach(menuLink => {
+         menuLink.addEventListener('click', onMenuLinkClick)
+      })
+      function onMenuLinkClick(e) {
+         const menuLink = e.target
+         if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const gotoBlock = document.querySelector(menuLink.dataset.goto)
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+            window.scrollTo({
+               top: gotoBlockValue,
+               behavior: 'smooth'
+            })
+            e.preventDefault()
+         }
+      }
+   }
+
+   // BURGER
+   if (iconMenu) {
+      iconMenu.onclick = (() => {
+         iconMenu.classList.toggle('_active')
+         menuBody.classList.toggle('_active')
+         document.body.classList.toggle('_lock')
+      })
+   }
+
+   // CLOSE MENU WHEN CLICK ON LINK
+   for (const link of menuLinks) {
+      link.onclick = (() => {
+         document.body.classList.remove('_lock')
+         iconMenu.classList.remove('_active')
+         menuBody.classList.remove('_active')
+      })
+   }
+   // IBG METHOD
+   // function ibg() {
+   //    let ibg = document.querySelectorAll(".ibg");
+   //    for (var i = 0; i < ibg.length; i++) {
+   //       if (ibg[i].querySelector('img')) {
+   //          ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
+   //       }
+   //    }
+   // }
+   // ibg();
+});
+
+// PRELOADER
 // let load = document.querySelector('.loading');
 // window.onload = (function () {
 //   load.classList.add('none');
 // });
-// //  DYNAMIC ADAPTIVE
-// // data - da="КУДА ПЕРЕМІСТИТИ(КЛАС),ЯКИМ ПО РАХУНКУ,ПРИ ЯКОМУ РОЗМІРІ ЕКРАНА"
+//  DYNAMIC ADAPTIVE
+// data - da="WHERE TO MOVE (CLASS), WHICH ACCOUNT, AT WHAT SCREEN SIZE"
 // (function () {
 //   var index;
 //   var daBreakpoint;
